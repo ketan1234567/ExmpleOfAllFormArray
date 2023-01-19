dba3bc9f35fb6c0bff2475ef47725813caab81c0
ketandeshmukh <deshmukhketan647@gmail.com>
2023-01-19 16:56:09 +0530

    "Done"

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Profile } from '../domain/profile';
+import { Technology } from '../domain/technology';
+import { User } from '../domain/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { } 
+
+  getProfiles():Profile[]{
+    let profiles=[
+      new Profile('dev','developer'),
+      new Profile('man','manager'),
+      new Profile('dir','director')
+
+  ]
+  return profiles;
+
+  }
+  getTechnologies():Technology[]{
+    
+  let technologies=[
+     new Technology(100,'java'),
+     new Technology(101,'angular'),
+     new Technology(103,'spring')
+
+  ]
+  return technologies;
+  }
+  createUser(user: User) {
+    //Log user data in console
+    console.log("User Name: " + user.userName);
+    console.log("Profile Id: " + user.profile.prId);
+    console.log("Profile Name: " + user.profile.prName);
+    for (let i = 0; i < user.technologies.length; i++) {
+        console.log("Technology Id: " + user.technologies[i].techId);
+        console.log("Technology Name: " + user.technologies[i].techName);
+    }
+}
+}
